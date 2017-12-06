0f081b3261b4ce9e4f5d37a83e582f4b88f59df6
bhavesh05 <bhavesh_vera@yahoo.co.in>
2017-12-06 13:47:33 -0500

    added senti

---
+++ senti.py
@@ -0,0 +1,24 @@
+import csv
+import os
+from textblob import TextBlob
+
+#infile = 'C:/Users/bhave/Downloads/smmPro/csv_files_pre/the_promise.csv'
+
+directory = os.path.join(os.getcwd(),"csv_files_pre")
+for file_name in os.listdir(directory):
+    file_path =  os.path.join(directory,file_name)
+    print(file_path)
+    with open(file_path, 'r', encoding="utf8") as csvfile:
+        rows = csv.reader(csvfile)
+        polarity = 0
+        row_count = 0
+        for row in rows:
+            row_count+=1
+            sentence = row[1]
+            blob = TextBlob(sentence)
+            polarity = polarity + (blob.sentiment.polarity)
+        average_polarity = (polarity/row_count)
+        fields = [file_name, average_polarity]
+        writer = csv.writer(open("polairity_table.csv", "a"), delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(fields)
+        print(average_polarity)
